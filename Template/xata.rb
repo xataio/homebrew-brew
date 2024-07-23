62ec7e79462a93292b2718ef870f3bf9e8ea6e3b
Alexis Rico <sferadev@gmail.com>
2024-07-23 10:25:05 +0200

    Remove autocomplete for now
    Signed-off-by: Alexis Rico <sferadev@gmail.com>

--- Template/xata.rb
+++ Template/xata.rb
@@ -28,24 +28,6 @@ class Xata < Formula
     inreplace "bin/xata", /^CLIENT_HOME=/, "export XATA_OCLIF_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
     libexec.install Dir["*"]
     bin.install_symlink libexec/"bin/xata"
-
-    bash_completion.install libexec/"autocomplete-scripts/brew/bash" => "xata"
-    zsh_completion.install libexec/"autocomplete-scripts/brew/zsh/_xata"
-  end
-
-  def caveats; <<~EOS
-    To use the Xata CLI's autocomplete --
-      Via homebrew's shell completion:
-        1) Follow homebrew's install instructions https://docs.brew.sh/Shell-Completion
-            NOTE: For zsh, as the instructions mention, be sure compinit is autoloaded
-                  and called, either explicitly or via a framework like oh-my-zsh.
-        2) Then run
-          $ xata autocomplete --refresh-cache
-      OR
-      Use our standalone setup:
-        1) Run and follow the install steps:
-          $ xata autocomplete
-  EOS
   end
 
   test do
