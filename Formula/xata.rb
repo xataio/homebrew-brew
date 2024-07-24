63cb38172b0a1928e890b14d5f4e4aef8da7050b
Xata <system@xata.io>
2024-07-24 08:44:17 +0000

    Add changes

--- Formula/xata.rb
+++ Formula/xata.rb
@@ -1,25 +1,25 @@
 class Xata < Formula
   desc Everything you need to get started with Xata
   homepage https://xata.io
-  url https://xata-cli-assets.s3.us-east-1.amazonaws.com/versions/2/1/xata-v2-1-darwin-x64.tar.xz
+  url "https://xata-cli-assets.s3.us-east-1.amazonaws.com/versions/2/1/xata-v2-1-darwin-x64.tar.xz"
   sha256 "3"
   version "2"
   version_scheme 1
 
   on_macos do
     on_arm do
-      url https://xata-cli-assets.s3.us-east-1.amazonaws.com/versions/2/1/xata-v2-1-darwin-x64.tar.xz
+      url "https://xata-cli-assets.s3.us-east-1.amazonaws.com/versions/2/1/xata-v2-1-darwin-arm64.tar.xz"
       sha256 "4"
     end
   end
 
   on_linux do
     on_intel do
-      url https://xata-cli-assets.s3.us-east-1.amazonaws.com/versions/2/1/xata-v2-1-darwin-x64.tar.xz
+      url "https://xata-cli-assets.s3.us-east-1.amazonaws.com/versions/2/1/xata-v2-1-linux-x64.tar.xz"
       sha256 "5"
     end
     on_arm do
-      url https://xata-cli-assets.s3.us-east-1.amazonaws.com/versions/2/1/xata-v2-1-darwin-x64.tar.xz
+      url "https://xata-cli-assets.s3.us-east-1.amazonaws.com/versions/2/1/xata-v2-1-linux-arm64.tar.xz"
       sha256 "6"
     end
   end
