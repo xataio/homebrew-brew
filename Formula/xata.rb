4cb11092452908109a9f59a3fe04f343cc1687c2
Xata <system@xata.io>
2024-07-24 08:32:56 +0000

    Add changes

--- Formula/xata.rb
+++ Formula/xata.rb
@@ -1,26 +1,26 @@
 class Xata < Formula
   desc Everything you need to get started with Xata
   homepage https://xata.io
-  url ${CLI_MAC_INTEL_DOWNLOAD_URL}
-  sha256 3
-  version 2
+  url "$CLI_MAC_INTEL_DOWNLOAD_URL"
+  sha256 "3"
+  version "2"
   version_scheme 1
 
   on_macos do
     on_arm do
-      url ${CLI_MAC_INTEL_DOWNLOAD_URL}
-      sha256 4
+      url "$CLI_MAC_INTEL_DOWNLOAD_URL"
+      sha256 "4"
     end
   end
 
   on_linux do
     on_intel do
-      url ${CLI_MAC_INTEL_DOWNLOAD_URL}
-      sha256 5
+      url "$CLI_MAC_INTEL_DOWNLOAD_URL"
+      sha256 "5"
     end
     on_arm do
-      url ${CLI_MAC_INTEL_DOWNLOAD_URL}
-      sha256 6
+      url "$CLI_MAC_INTEL_DOWNLOAD_URL"
+      sha256 "6"
     end
   end
 
