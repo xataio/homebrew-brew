2bb23346b17e703c1c9a15f4fa78a13e47de4284
Xata <system@xata.io>
2024-07-24 10:01:36 +0000

    Update dependencies to version 0.15.15

--- Formula/xata.rb
+++ Formula/xata.rb
@@ -1,26 +1,26 @@
 class Xata < Formula
   desc "Everything you need to get started with Xata"
   homepage "https://xata.io"
-  url "https://xata-cli-assets.s3.us-east-1.amazonaws.com/versions/0.15.15//xata-v0.15.15--darwin-x64.tar.xz"
+  url "https://xata-cli-assets.s3.us-east-1.amazonaws.com/versions/0.15.15/34c20746/xata-v0.15.15-34c20746-darwin-x64.tar.xz"
   sha256 ","
   version "0.15.15"
   version_scheme 1
 
   on_macos do
     on_arm do
-      url "https://xata-cli-assets.s3.us-east-1.amazonaws.com/versions/0.15.15//xata-v0.15.15--darwin-arm64.tar.xz"
-      sha256 "24292ec373b4153eb889d22d0de9b93f2ba524df"
+      url "https://xata-cli-assets.s3.us-east-1.amazonaws.com/versions/0.15.15/34c20746/xata-v0.15.15-34c20746-darwin-arm64.tar.xz"
+      sha256 "140144baca3b6a78b706a459248f2dfd53efd0da"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://xata-cli-assets.s3.us-east-1.amazonaws.com/versions/0.15.15//xata-v0.15.15--linux-x64.tar.xz"
-      sha256 "65fecdf847d254bdf80bc040d354e77be5a3024a8cc982ff811144b42648d655  xata-v0.15.15-bd033e61-linux-arm.tar.xz,"
+      url "https://xata-cli-assets.s3.us-east-1.amazonaws.com/versions/0.15.15/34c20746/xata-v0.15.15-34c20746-linux-x64.tar.xz"
+      sha256 "ba83dbfc51e79bc53e11504675eadf3e305d993a44730b36b0e1cbf4f683e424,"
     end
     on_arm do
-      url "https://xata-cli-assets.s3.us-east-1.amazonaws.com/versions/0.15.15//xata-v0.15.15--linux-arm64.tar.xz"
-      sha256 "9c091dc3e3bd080feedb0720363790bbdbec7cb229991724bd73c170301f68c1  xata-v0.15.15-bd033e61-linux-arm64.tar.xz"
+      url "https://xata-cli-assets.s3.us-east-1.amazonaws.com/versions/0.15.15/34c20746/xata-v0.15.15-34c20746-linux-arm64.tar.xz"
+      sha256 "604f09503ff638bbfe75604cc4dcbaac6d9ee0f6602459dc2aec27e7b0572114"
     end
   end
 
