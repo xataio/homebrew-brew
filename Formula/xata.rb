3be1b8401d036bc788dfa1d76b88a6079f7a789e
Xata <system@xata.io>
2024-07-24 08:42:20 +0000

    Add changes

--- Formula/xata.rb
+++ Formula/xata.rb
@@ -1,25 +1,25 @@
 class Xata < Formula
   desc Everything you need to get started with Xata
   homepage https://xata.io
-  url "$CLI_MAC_INTEL_DOWNLOAD_URL"
+  url https://xata-cli-assets.s3.us-east-1.amazonaws.com/versions/2/1/xata-v2-1-darwin-x64.tar.xz
   sha256 "3"
   version "2"
   version_scheme 1
 
   on_macos do
     on_arm do
-      url "$CLI_MAC_INTEL_DOWNLOAD_URL"
+      url https://xata-cli-assets.s3.us-east-1.amazonaws.com/versions/2/1/xata-v2-1-darwin-x64.tar.xz
       sha256 "4"
     end
   end
 
   on_linux do
     on_intel do
-      url "$CLI_MAC_INTEL_DOWNLOAD_URL"
+      url https://xata-cli-assets.s3.us-east-1.amazonaws.com/versions/2/1/xata-v2-1-darwin-x64.tar.xz
       sha256 "5"
     end
     on_arm do
-      url "$CLI_MAC_INTEL_DOWNLOAD_URL"
+      url https://xata-cli-assets.s3.us-east-1.amazonaws.com/versions/2/1/xata-v2-1-darwin-x64.tar.xz
       sha256 "6"
     end
   end
