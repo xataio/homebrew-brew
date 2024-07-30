3c0f1b4065ded17887c6b301d93cd715efe31ea7
Xata <system@xata.io>
2024-07-30 09:25:40 +0000

    Update dependencies to version 0.16.1

--- Formula/xata.rb
+++ Formula/xata.rb
@@ -1,26 +1,26 @@
 class Xata < Formula
   desc "Everything you need to get started with Xata"
   homepage "https://xata.io"
-  url "https://xata-cli-assets.s3.us-east-1.amazonaws.com/versions/0.16.0/9b2b2e9b/xata-v0.16.0-9b2b2e9b-darwin-x64.tar.xz"
-  sha256 "67a7032c586dad347c907358bd012191869f635a69364d5f5d0341ac4d86bc0d"
-  version "0.16.0"
+  url "https://xata-cli-assets.s3.us-east-1.amazonaws.com/versions/0.16.1/0c18aa58/xata-v0.16.1-0c18aa58-darwin-x64.tar.xz"
+  sha256 "7210ff11f128bbf9e852fca98ef27bb5537ea1042871198aeb53b6ec61d95d9c"
+  version "0.16.1"
   version_scheme 1
 
   on_macos do
     on_arm do
-      url "https://xata-cli-assets.s3.us-east-1.amazonaws.com/versions/0.16.0/9b2b2e9b/xata-v0.16.0-9b2b2e9b-darwin-arm64.tar.xz"
-      sha256 "ab30a8665e74517cd7f49961ff10c3624cbfc3e80231aba5f45fbd302485d23f"
+      url "https://xata-cli-assets.s3.us-east-1.amazonaws.com/versions/0.16.1/0c18aa58/xata-v0.16.1-0c18aa58-darwin-arm64.tar.xz"
+      sha256 "e5ce1132dd05f2b17f3db6e802a47893dcb17b5d996419805e785735508a9435"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://xata-cli-assets.s3.us-east-1.amazonaws.com/versions/0.16.0/9b2b2e9b/xata-v0.16.0-9b2b2e9b-linux-x64.tar.xz"
-      sha256 "323273852040f26c13ef8c7f7b96fb44c632233d39664dbdf3afa6143c65ab04"
+      url "https://xata-cli-assets.s3.us-east-1.amazonaws.com/versions/0.16.1/0c18aa58/xata-v0.16.1-0c18aa58-linux-x64.tar.xz"
+      sha256 "299774c7714b6e02e362cc307e2d4b7cd349d252c46daec035e09342a576deec"
     end
     on_arm do
-      url "https://xata-cli-assets.s3.us-east-1.amazonaws.com/versions/0.16.0/9b2b2e9b/xata-v0.16.0-9b2b2e9b-linux-arm64.tar.xz"
-      sha256 "cf48d1d164cd87f2d22686fe803af73e792f42d1778d0f42ab075f9f6d6e1098"
+      url "https://xata-cli-assets.s3.us-east-1.amazonaws.com/versions/0.16.1/0c18aa58/xata-v0.16.1-0c18aa58-linux-arm64.tar.xz"
+      sha256 "1fb10b1df320f1118760c69ba7fe4d3623b04f16c527a0263dadd6c98fc61728"
     end
   end
 
