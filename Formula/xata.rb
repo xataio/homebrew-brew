a8895c77ac50b4fbdaca91d015f4e45ebdee2975
Xata <system@xata.io>
2024-09-19 17:38:58 +0000

    Update dependencies to version 0.16.10

--- Formula/xata.rb
+++ Formula/xata.rb
@@ -1,26 +1,26 @@
 class Xata < Formula
   desc "Everything you need to get started with Xata"
   homepage "https://xata.io"
-  url "https://xata-cli-assets.s3.us-east-1.amazonaws.com/versions/0.16.1/0c18aa58/xata-v0.16.1-0c18aa58-darwin-x64.tar.xz"
-  sha256 "7210ff11f128bbf9e852fca98ef27bb5537ea1042871198aeb53b6ec61d95d9c"
-  version "0.16.1"
+  url "https://xata-cli-assets.s3.us-east-1.amazonaws.com/versions/0.16.10/de0148dd/xata-v0.16.10-de0148dd-darwin-x64.tar.xz"
+  sha256 "7ffb57cfdc1db6b1f4138ee440bc478571639e6e4492d89e01857d1bc5334193"
+  version "0.16.10"
   version_scheme 1
 
   on_macos do
     on_arm do
-      url "https://xata-cli-assets.s3.us-east-1.amazonaws.com/versions/0.16.1/0c18aa58/xata-v0.16.1-0c18aa58-darwin-arm64.tar.xz"
-      sha256 "e5ce1132dd05f2b17f3db6e802a47893dcb17b5d996419805e785735508a9435"
+      url "https://xata-cli-assets.s3.us-east-1.amazonaws.com/versions/0.16.10/de0148dd/xata-v0.16.10-de0148dd-darwin-arm64.tar.xz"
+      sha256 "fd1f4c436b968b1e96aadf0ac902759acc5bb47bfb371754819fb04828d886d4"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://xata-cli-assets.s3.us-east-1.amazonaws.com/versions/0.16.1/0c18aa58/xata-v0.16.1-0c18aa58-linux-x64.tar.xz"
-      sha256 "299774c7714b6e02e362cc307e2d4b7cd349d252c46daec035e09342a576deec"
+      url "https://xata-cli-assets.s3.us-east-1.amazonaws.com/versions/0.16.10/de0148dd/xata-v0.16.10-de0148dd-linux-x64.tar.xz"
+      sha256 "80841025ebdc03937d58d950e35eedfc8dbef7953830c34064ec598abe3597ce"
     end
     on_arm do
-      url "https://xata-cli-assets.s3.us-east-1.amazonaws.com/versions/0.16.1/0c18aa58/xata-v0.16.1-0c18aa58-linux-arm64.tar.xz"
-      sha256 "1fb10b1df320f1118760c69ba7fe4d3623b04f16c527a0263dadd6c98fc61728"
+      url "https://xata-cli-assets.s3.us-east-1.amazonaws.com/versions/0.16.10/de0148dd/xata-v0.16.10-de0148dd-linux-arm64.tar.xz"
+      sha256 "a44088b321a79d722c27fbe6bbad99dfb5fce9fd480691c0db9e897de791f016"
     end
   end
 
